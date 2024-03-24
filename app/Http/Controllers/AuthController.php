c3dfd96cde83cb834b8d510cfcbd0d8ce40da475
anasmak04 <anasdev8@gmail.com>
2024-03-24 23:26:55 +0000

    add login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -11,8 +12,8 @@ class AuthController extends Controller
 {
     //
 
-    public function register(RegisterRequest $request)
-    {
+        public function register(RegisterRequest $request)
+        {
         try {
             $userRequest = $request->only("name", "email", "password");
             $userRequest["password"] = Hash::make($userRequest["password"]);
@@ -26,6 +27,28 @@ class AuthController extends Controller
                 "token" => $token
             ]);
         } catch (\Exception $e) {
+            return response()->json(["message" => "User registration failed", "error" => $e->getMessage()], 500);
+        }
+    }
+
+
+    public function login(LoginRequest $request)
+    {
+        try{
+            $userrequest = $request->only("email", "password");
+            $user = User::where("email", $userrequest["email"])->first();
+
+            if(!$user || !  Hash::check($userrequest["password"], $user->password)){
+                return response()->json(["message" => "unauthorized"]);
+            }
+
+            $token = $user->createToken("auth_token")->plainTextToken;
+
+            return response()->json([
+                "message" => "Logged in successfully",
+                "token" => $token
+            ]);
+        }catch (\Exception $e) {
             return response()->json(["message" => "User registration failed", "error" => $e->getMessage()], 500);
         }
     }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::post("/register", [\App\Http\Controllers\AuthController::class, "register"]);
+Route::post("/login", [\App\Http\Controllers\AuthController::class, "login"]);
+
